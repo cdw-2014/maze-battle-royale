d32d07de264d4063039f92d59e94251219189787
CJ Weaver <cdw2014@gmail.com>
2019-12-29 22:35:19 -0500

    almost finished. sorry for the bad commit message

--- public/sketch.js
+++ public/sketch.js
@@ -10,201 +10,254 @@ var sets = [];
 var wallsRemoved = 0;
 var showControls = true;
 var host = false;
-var gameCode = "";
+var gameCode = '';
 var inGame = false;
 var isGenerated = false;
+var isMazeComplete = false;
+var displayName = '';
+var leaderboard = [];
 
 var socket;
 let waitingP;
+let leaderboardP;
 
 function setup() {
+	socket = io.connect('http://localhost:3000');
+	socket.on('playerJoined', function(player) {
+		console.log(player);
+	});
+	socket.on('updateMaze', function(newGrid) {
+		console.log('TEST UPDATE MAZE');
+		grid = [];
+		cursorX = 0;
+		cursorY = 0;
+		sets = [];
+		isGenerated = false;
+		isMazeComplete = false;
+		isFinished = false;
+		wallsRemoved = 0;
+		for (let cell of newGrid) {
+			grid.push(new Cell(cell.i, cell.j, cell.walls));
+		}
+		if (waitingP !== undefined) {
+			waitingP.remove();
+		}
+		inGame = true;
+		isGenerated = true;
+	});
+	socket.on('updateLeaderboard', function(data) {
+		let { newLeaderboard, isFinished } = data;
+		leaderboard = newLeaderboard;
+		if (leaderboardP !== undefined) {
+			leaderboardP.remove();
+		}
+		leaderboardP = createP(leaderboardToString());
+		console.log(newLeaderboard, isFinished);
+		if (host && isFinished) {
+			console.log('test');
+			reset();
+		}
+	});
+	createCanvas(500, 500);
+	cols = floor(width / w);
+	rows = floor(height / w);
+	let enterName = createInput().attribute('placeholder', 'Enter Display Name');
+	enterName.position(50, 50);
+	let createLobbyButton = createButton('Create Lobby');
+	createLobbyButton.position(50, 100);
+	let lobbyInput = createInput().attribute('placeholder', 'Enter Code');
+	lobbyInput.position(50, 175);
+	let joinButton = createButton('Join');
+	joinButton.position(50, 200);
+	let joinError;
 
-    socket = io.connect('http://localhost:3000');
-    socket.on('playerJoined', function(player) {
-        console.log(player);
-    });
-    socket.on('updateMaze', function(newGrid) {
-        grid = [];
-        for (let cell of newGrid) {
-            grid.push(new Cell(cell.i, cell.j, cell.walls));
-        }
-        if(waitingP !== undefined) {
-            waitingP.remove();
-        }
-        inGame = true;
-        isGenerated = true;
-    });
-    createCanvas(500, 500);
-    cols = floor(width/w);
-    rows = floor(height/w);
-    let createLobbyButton = createButton("Create Lobby");
-    createLobbyButton.position(50,50);
-    let lobbyInput = createInput("Enter Code");
-    lobbyInput.position(50, 125);
-    let joinButton = createButton("Join");
-    joinButton.position(50, 150);
+	joinButton.mousePressed(() => {
+		displayName = enterName.value();
+		socket.emit('joinLobby', { code: lobbyInput.value(), name: displayName }, function(joined, err) {
+			console.log(err);
+			if (joined) {
+				gameCode = lobbyInput.value();
+				createLobbyButton.remove();
+				lobbyInput.remove();
+				joinButton.remove();
+				enterName.remove();
+				waitingP = createElement('h1', 'Waiting for host to start game...');
+				waitingP.position(50, 50);
+			}
+		});
+	});
 
-    joinButton.mousePressed(() => {
-        socket.emit('joinLobby', lobbyInput.value(), function(joined) {
-            if(joined) {
-                createLobbyButton.remove();
-                lobbyInput.remove();
-                joinButton.remove();
-                waitingP = createElement("h1","Waiting for  host to start game...");
-                waitingP.position(50,50);
-            }
-        });
-    })
-
-    createLobbyButton.mousePressed(() => {
-        createLobbyButton.remove();
-        lobbyInput.remove();
-        joinButton.remove();
-        createLobby();
-    });
+	createLobbyButton.mousePressed(() => {
+		displayName = enterName.value();
+		createLobbyButton.remove();
+		lobbyInput.remove();
+		joinButton.remove();
+		enterName.remove();
+		createLobby();
+	});
 }
 
 function createLobby() {
-    host = true;
-    let code = char(random(65,91)) + char(random(65,91)) + char(random(65,91)) + char(random(65,91)) +char(random(65,91)) + char(random(65,91));
-    let lobbyCode = createP(`Lobby Code: ${code}`);
-    lobbyCode.position(50,50);
-    let startGameButton = createButton("Start Game");
-    startGameButton.position(50, 125);
-    socket.emit('createLobby', code);
-    gameCode = code;
-    startGameButton.mousePressed(() => {
-        lobbyCode.remove();
-        startGameButton.remove();
-        inGame = true;
-        reset();
-        generateMaze();
-    });
+	host = true;
+	let code =
+		char(random(65, 91)) +
+		char(random(65, 91)) +
+		char(random(65, 91)) +
+		char(random(65, 91)) +
+		char(random(65, 91)) +
+		char(random(65, 91));
+	let lobbyCode = createP(`Lobby Code: ${code}`);
+	lobbyCode.position(50, 50);
+	let startGameButton = createButton('Start Game');
+	startGameButton.position(50, 125);
+	socket.emit('createLobby', { code: code, name: displayName });
+	gameCode = code;
+	startGameButton.mousePressed(() => {
+		lobbyCode.remove();
+		startGameButton.remove();
+		inGame = true;
+		reset();
+	});
 }
 
 function reset() {
-  grid = [];
-  cursorX = 0;
-  cursorY = 0;
-  sets = [];
-  wallsRemoved = 0;
-  for (var j = 0; j < rows; j++) {
-    for (var i = 0; i < cols; i++) {
-      var cell = new Cell(i, j);
-      grid.push(cell);
-      sets.push([cell.id])
-    }
-  }
-  available = grid;
-  current = grid[int(random(0,(rows*cols)))];
+	grid = [];
+	cursorX = 0;
+	cursorY = 0;
+	sets = [];
+	isGenerated = false;
+	isMazeComplete = false;
+	isFinished = false;
+	wallsRemoved = 0;
+	for (var j = 0; j < rows; j++) {
+		for (var i = 0; i < cols; i++) {
+			var cell = new Cell(i, j);
+			grid.push(cell);
+			sets.push([
+				cell.id
+			]);
+		}
+	}
+	available = grid;
+	current = grid[int(random(0, rows * cols))];
 }
 
-function generateMaze() {
-    
+function finishedMaze(tries = 0) {
+	if (tries > 10) return;
+	socket.emit('finishedMaze', gameCode, function(registered) {
+		registered ? (isMazeComplete = true) : finishedMaze(tries + 1);
+	});
 }
 
 function draw() {
-    if(inGame) {
-        background(51);
-        for (var i = 0; i < grid.length; i++) {
-            grid[i].show();
-        }
-        if(isGenerated) {
-            selected = grid.filter(cell => (cell.i === cursorX && cell.j === cursorY))[0];
-            selected.highlight();
-            grid[grid.length-1].highlight(true);
-            if(grid[grid.length-1].id === selected.id) reset();
-        } else {
-            if (wallsRemoved < cols*rows - 1) {
-                if (!current.isFinished) {
-                // CHOOSE RANDOM NEIGHBOR
-                var neighbor = current.randomNeighbor();
-                var mergedSet, removedSet, removedIndex;
-                if (neighbor) {
-                    // FIND THE SETS THAT CURRENT AND NEIGHBOR ARE IN
-                    for (var i = 0; i < sets.length; i++) {
-                    if (sets[i].includes(current.id)) {
-                    mergedSet = sets[i];
-                    }
-                    else if (sets[i].includes(neighbor.id)) {
-                    removedSet = sets[i];
-                    removedIndex = i;
-                    }
-                    }
-                    // ADD NEIGHBOR TO CURRENT SET (UNION THE CELLS) AND DELETE NEIGHBOR'S SET
-                    if(removedSet !== undefined) {
-                    for (var i = 0; i < removedSet.length; i++) {
-                        mergedSet.push(removedSet[i]);
-                    }
-                    sets.splice(removedIndex,1);
-                    // REMOVE WALLS FROM BETWEEN CURRENT AND NEIGHBOR
-                    removeWalls(current, neighbor);
-                    }
-                } else if (sets.length > 1) {
-                    current.isFinished = true;
-                    available = available.filter(cell => cell.id !== current.id);
-                }
-                }
-                // GET NEW CURRENT (RANDOM)
-                current = available[int(random(0,available.length))];
-            } else {
-                socket.emit('generation', {grid: grid, code: gameCode});
-                isGenerated = true;
-            }
-        }
-    }
+	if (inGame && !isMazeComplete) {
+		background(51);
+		for (var i = 0; i < grid.length; i++) {
+			grid[i].show();
+		}
+		if (isGenerated) {
+			selected = grid.filter((cell) => cell.i === cursorX && cell.j === cursorY)[0];
+			selected.highlight();
+			grid[grid.length - 1].highlight(true);
+			if (grid[grid.length - 1].id === selected.id) {
+				finishedMaze();
+			}
+		} else {
+			if (wallsRemoved < cols * rows - 1) {
+				if (!current.isFinished) {
+					// CHOOSE RANDOM NEIGHBOR
+					var neighbor = current.randomNeighbor();
+					var mergedSet, removedSet, removedIndex;
+					if (neighbor) {
+						// FIND THE SETS THAT CURRENT AND NEIGHBOR ARE IN
+						for (var i = 0; i < sets.length; i++) {
+							if (sets[i].includes(current.id)) {
+								mergedSet = sets[i];
+							} else if (sets[i].includes(neighbor.id)) {
+								removedSet = sets[i];
+								removedIndex = i;
+							}
+						}
+						// ADD NEIGHBOR TO CURRENT SET (UNION THE CELLS) AND DELETE NEIGHBOR'S SET
+						if (removedSet !== undefined) {
+							for (var i = 0; i < removedSet.length; i++) {
+								mergedSet.push(removedSet[i]);
+							}
+							sets.splice(removedIndex, 1);
+							// REMOVE WALLS FROM BETWEEN CURRENT AND NEIGHBOR
+							removeWalls(current, neighbor);
+						}
+					} else if (sets.length > 1) {
+						current.isFinished = true;
+						available = available.filter((cell) => cell.id !== current.id);
+					}
+				}
+				// GET NEW CURRENT (RANDOM)
+				current = available[int(random(0, available.length))];
+			} else {
+				socket.emit('generation', { grid: grid, code: gameCode });
+				isGenerated = true;
+			}
+		}
+	}
 }
 
 // cell.i == x coord
 //cell.j == y coord
 function keyPressed() {
-    if (key == 'R' || key =='r') {
-      cursorX = 0;
-      cursorY = 0;
-    }
-    else if ((key == 'W' || key == 'w') && cursorY > 0) {
-      if(!selected.walls[0]) {
-        cursorY--;
-      }
-    } else if ((key == 'A' || key == 'a') && cursorX > 0) {
-      if(!selected.walls[3]) {
-        cursorX--;
-      }
-    } else if ((key == 'S' || key == 's') && cursorY < rows-1) {
-      if(!selected.walls[2]) {
-        cursorY++;
-      }
-    } else if ((key == 'D' || key == 'd') && cursorX < cols-1) {
-      if(!selected.walls[1]) {
-        cursorX++;
-      }
-    }
-  }
-
-function index(i, j) {
-  if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
-    return -1;
-  }
-  return i + j * cols;
+	if (key == 'R' || key == 'r') {
+		cursorX = 0;
+		cursorY = 0;
+	} else if ((key == 'W' || key == 'w') && cursorY > 0) {
+		if (!selected.walls[0]) {
+			cursorY--;
+		}
+	} else if ((key == 'A' || key == 'a') && cursorX > 0) {
+		if (!selected.walls[3]) {
+			cursorX--;
+		}
+	} else if ((key == 'S' || key == 's') && cursorY < rows - 1) {
+		if (!selected.walls[2]) {
+			cursorY++;
+		}
+	} else if ((key == 'D' || key == 'd') && cursorX < cols - 1) {
+		if (!selected.walls[1]) {
+			cursorX++;
+		}
+	}
 }
 
+function index(i, j) {
+	if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+		return -1;
+	}
+	return i + j * cols;
+}
+
+function leaderboardToString() {
+	let string = '';
+	for (let leader of leaderboard) {
+		string += leader.name + ': ' + leader.score + '\n';
+	}
+	return string;
+}
 
 function removeWalls(a, b) {
-  var x = a.i - b.i;
-  if (x === 1) {
-    a.walls[3] = false;
-    b.walls[1] = false;
-  } else if (x === -1) {
-    a.walls[1] = false;
-    b.walls[3] = false;
-  }
-  var y = a.j - b.j;
-  if (y === 1) {
-    a.walls[0] = false;
-    b.walls[2] = false;
-  } else if (y === -1) {
-    a.walls[2] = false;
-    b.walls[0] = false;
-  }
-  wallsRemoved++;
+	var x = a.i - b.i;
+	if (x === 1) {
+		a.walls[3] = false;
+		b.walls[1] = false;
+	} else if (x === -1) {
+		a.walls[1] = false;
+		b.walls[3] = false;
+	}
+	var y = a.j - b.j;
+	if (y === 1) {
+		a.walls[0] = false;
+		b.walls[2] = false;
+	} else if (y === -1) {
+		a.walls[2] = false;
+		b.walls[0] = false;
+	}
+	wallsRemoved++;
 }
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000, listen);
 
 function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://' + host + ':' + port);
 }
 
 app.use(express.static('public'));
@@ -13,46 +13,98 @@ var io = require('socket.io')(server);
 
 let lobbies = [];
 
-io.sockets.on('connection',
-  function (socket) {
-    console.log("Client: " + socket.id);
-    socket.on('createLobby',
-      function(code) {
-        console.log("Received: 'createLobby' " + code + " " + socket.id);
-        lobbies.push({code: code, host: socket.id, players: [socket.id]});
-        socket.join(code);
-        console.log(lobbies);
-      }
-    );
-    socket.on('joinLobby',
-      function(code, callback) {
-        console.log("Received: 'joinLobby' " + code + " " + socket.id);
-        for(let lobby of lobbies) {
-            if(lobby.code === code) {
-                lobby.players.push(socket.id);
-                console.log(lobbies);
-                socket.join(code);
-                io.to(code).emit('playerJoined', socket.id + " has joined the lobby.");
-                callback(true);
-            }
-        }
-        callback(false);
-      }
-    );
-    socket.on('generation',
-      function(data) {
-        let { grid, code } = data;
-        console.log("Received: 'generation' " + grid + " " + code);
-        io.to(code).emit('updateMaze', grid);
-      }
-    );
-    socket.on('disconnect', function() {
-      console.log(`${socket.id} has disconnected`);
-      lobbies = lobbies.filter(lobby => lobby.host !== socket.id);
-      for(let lobby of lobbies) {
-          lobby.players = lobby.players.filter(player => player !== socket.id)
-      }
-      console.log(lobbies);
-    });
-  }
-);
+io.sockets.on('connection', function(socket) {
+	console.log('Client: ' + socket.id);
+	socket.on('createLobby', function(data) {
+		let { code, name } = data;
+		if (name.length == 0) {
+			name = 'Player1';
+		}
+		console.log("Received: 'createLobby' " + code + ' ' + socket.id);
+		lobbies.push({
+			code    : code,
+			host    : socket.id,
+			players : [
+				{ name: name, id: socket.id }
+			]
+		});
+		socket.join(code);
+	});
+	socket.on('joinLobby', function(data, callback) {
+		let { code, name } = data;
+		console.log("Received: 'joinLobby' " + code + ' ' + socket.id);
+		for (let lobby of lobbies) {
+			if (lobby.code === code) {
+				let nameExists = false;
+				if (name.length == 0) {
+					name = 'Player' + String(lobby.players.length + 1);
+				}
+				for (let player of lobby.players) {
+					if (player.name === name) {
+						nameExists = true;
+					}
+				}
+				if (!nameExists) {
+					lobby.players.push({ name: name, id: socket.id });
+					socket.join(code);
+					io.to(code).emit('playerJoined', socket.id + ' has joined the lobby.');
+					callback(true);
+				} else {
+					callback(false, 'A player with the same name is already in this lobby!');
+				}
+			}
+		}
+		callback(false, 'Invalid Lobby Code!');
+	});
+	socket.on('generation', function(data) {
+		let { grid, code } = data;
+		console.log("Received: 'generation' " + code);
+		io.to(code).emit('updateMaze', grid);
+	});
+	socket.on('finishedMaze', function(code, callback) {
+		// FIND CORRECT LOBBY
+		for (let lobby of lobbies) {
+			if (lobby.code === code) {
+				// CREATE RANKING ORDER THAT RESETS AFTER EACH MAZE
+				if (!lobby.hasOwnProperty('mazeRank')) {
+					lobby.mazeRank = [];
+				}
+				// PUSH PLAYERS TO MAZE RANKING AS THEY FINISH
+				lobby.mazeRank.push(socket.id);
+				// CREATE LEADERBOARD (ONLY FOR FIRST MAZE) AND INITIATE SCORES TO ZERO
+				if (!lobby.hasOwnProperty('leaderboard')) {
+					lobby.leaderboard = [];
+					for (let player of lobby.players) {
+						lobby.leaderboard.push({ name: player.name, score: 0 });
+					}
+				}
+				// FIND PLAYER NAME FROM SOCKET ID
+				for (let player of lobby.players) {
+					if (player.id === socket.id) {
+						for (let leader of lobby.leaderboard) {
+							if (leader.name === player.name) {
+								// INCREMENT SCORE FOR PLAYER
+								leader.score += 20 * (lobby.players.length / lobby.mazeRank.length);
+							}
+						}
+					}
+				}
+				lobby.leaderboard = lobby.leaderboard.sort((a, b) => b.score - a.score);
+				let isFinished = lobby.mazeRank.length >= lobby.players.length;
+				if (isFinished) {
+					delete lobby.mazeRank;
+				}
+				io.to(code).emit('updateLeaderboard', { newLeaderboard: lobby.leaderboard, isFinished });
+				callback(true);
+			}
+		}
+		callback(false);
+	});
+	socket.on('disconnect', function() {
+		console.log(`${socket.id} has disconnected`);
+		lobbies = lobbies.filter((lobby) => lobby.host !== socket.id);
+		for (let lobby of lobbies) {
+			lobby.players = lobby.players.filter((player) => player !== socket.id);
+		}
+	});
+});
